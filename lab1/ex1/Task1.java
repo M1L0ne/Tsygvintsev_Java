39245419ac41e4db60efa7d51adaabd9ce7a7446
M1L0ne <m1l0ne@mail.ru>
2025-09-29 19:51:20 +0500

    second task done

--- lab1/ex1/Task1.java
+++ lab1/ex1/Task1.java
@@ -1,34 +1,73 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Task1 {
-    public void main(String[] args) {       
-        //int x = 4568;
-        // System.out.println(sumLastNums(x));
+    public void main(String[] args) {
+        char xChar;
+        int a, b, x;
+        System.out.println("Введите номер задания (от 1 до 5): ");
+        Scanner sc = new Scanner(System.in);
+        int taskNum = sc.nextInt();
 
-        // int x = 3;
-        // System.out.println(isPositive(x));
+        switch(taskNum){
+            case 1:
+                System.out.println("Введите число: ");
+                try {
+                    x = sc.nextInt();
+                    System.out.println("Сумма его последних цифр: " + sumLastNums(x));
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введено не число.");
+                }
+                break;
+            case 2:
+                System.out.println("Введите число: ");
+                try {
+                    x = sc.nextInt();
+                    System.out.println("Число положительное? " + isPositive(x));
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введено не число.");
+                }
+                break;
+            case 3:
+                System.out.println("Введите букву: ");
+                try {
+                    xChar = sc.nextLine().charAt(0);
+                    System.out.println(isUpperCase(xChar));
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введена не буква.");
+                }
+                break;
+            case 4:
+                System.out.println("Введите 2 числа через Enter: ");
+                try {
+                    a = sc.nextInt();
+                    b = sc.nextInt();
+                    System.out.println("Любое из принятых чисел делит другое нацело? " + isDivisor(a, b));
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введено не число.");
+                }
+                break;
+            case 5:
+                System.out.println("Вводите числа через Enter. Для завершения введите '0' после первого числа: ");
+                try {
+                    a = sc.nextInt();
+                    do {
+                        b = sc.nextInt();
+                        a = lastNumSum(a, b);
+                    } while (b != 0);
 
-        // char x = 'A';
-        // System.out.println(isUpperCase(x));
-
-        // int a = 6;
-        // int b = 3;
-        // System.out.println(isDivisor(a, b));
-
-        int a = 5;
-        int b = 11;
-        a = lastNumSum(a, b);
-        b = 123;
-        a = lastNumSum(a, b);
-        b = 14;
-        a = lastNumSum(a, b);
-        b = 1;
-        a = lastNumSum(a, b);
-        System.out.println("Итого " + a);
+                    System.out.println("Итого " + a);
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введено не число.");
+                }
+                break;
+        }
     }
 
     public int sumLastNums(int x) {
         if (x / 100 != 0) {
             int lastDigit = x % 10;
-            int secondLastDigit = lastDigit % 10;
+            int secondLastDigit = (x / 10) % 10;
 
             int y = lastDigit + secondLastDigit;
             return y;
--- lab1/ex1/Task2.java
+++ lab1/ex1/Task2.java
@@ -1,10 +1,61 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Task2 {
     public void main(String[] args) {
-        // int x = 8;
-        // int y = 0;
-        // System.out.println(safeDiv(x, y));
+        String xString;
+        int a, b, x, y, z;
+        System.out.println("Введите номер задания (от 1 до 5): ");
+        Scanner sc = new Scanner(System.in);
+        int taskNum = sc.nextInt();
 
-
+        switch(taskNum) {
+            case 1:
+                System.out.println("Введите 2 числа через Enter: ");
+                try {
+                    x = sc.nextInt();
+                    y = sc.nextInt();
+                    System.out.println("Деление x на y: " + safeDiv(x, y));
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введено не число.");
+                }
+                break;
+            case 2:
+                System.out.println("Введите 2 числа через Enter: ");
+                try {
+                    x = sc.nextInt();
+                    y = sc.nextInt();
+                    System.out.println("Числа со знаком сравнения: " + makeDecision(x, y));
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введено не число.");
+                }
+                break;
+            case 3:
+                System.out.println("Введите 3 числа через Enter: ");
+                try {
+                    x = sc.nextInt();
+                    y = sc.nextInt();
+                    z = sc.nextInt();
+                    System.out.println("Из двух чисел можно получить третье? " + sum3(x, y, z));
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введено не число.");
+                }
+                break;
+            case 4:
+                System.out.println("Введите число: ");
+                try {
+                    x = sc.nextInt();
+                    System.out.println("Число в годах: " + age(x));
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введено не число.");
+                }
+                break;
+            case 5:
+                System.out.println("Введите день недели: ");
+                xString = sc.next();
+                printDays(xString);
+                break;
+        }
     }
 
     public double safeDiv(int x, int y) {
@@ -16,6 +67,76 @@ public class Task2 {
     }
 
     public String makeDecision(int x, int y) {
-        return "test";
+        if (x > y) {
+            return (x + " > " + y);
+        }
+        else if (x < y) {
+            return (x + " < " + y);
+        }
+        else {
+            return (x + " == " + y);
+        }
+    }
+
+    public boolean sum3(int x, int y, int z) {
+        if (x + y == z || x + z == y || y + z == x) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public String age(int x) {
+        int divX = x % 10;
+        if (divX == 1) {
+            return (x + " год");
+        }
+        else if ((divX >= 2 && divX <= 4) && (x < 12 || x > 14)) {
+            return (x + " года");
+        }
+        else {
+            return (x + " лет");
+        }
+    }
+
+    public void printDays(String x) {
+        String[] days = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+        switch (x) {
+            case "понедельник":
+                for (int i = 0; i < days.length; i++) {
+                    System.out.println(days[i]);
+                }
+                break;
+            case "вторник":
+                for (int i = 1; i < days.length; i++) {
+                    System.out.println(days[i]);
+                }
+                break;
+            case "среда":
+                for (int i = 2; i < days.length; i++) {
+                    System.out.println(days[i]);
+                }
+                break;
+            case "четверг":
+                for (int i = 3; i < days.length; i++) {
+                    System.out.println(days[i]);
+                }
+                break;
+            case "пятница":
+                for (int i = 4; i < days.length; i++) {
+                    System.out.println(days[i]);
+                }
+                break;
+            case "суббота":
+                for (int i = 5; i < days.length; i++) {
+                    System.out.println(days[i]);
+                }
+                break;
+            case "воскресенье":
+                System.out.println(days[6]);
+                break;
+            default:
+                System.out.println("это не день недели");
+        }
     }
 }
